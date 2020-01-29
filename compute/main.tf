7e4535ef19aa9e636c57e3730538c2af3d0c196c
EC2 Default User <ec2-user@ip-172-31-24-87.us-west-2.compute.internal>
2020-01-29 23:41:54 +0000

    adding template files

--- compute/main.tf
+++ compute/main.tf
@@ -19,6 +19,15 @@ resource "aws_key_pair" "tf_auth" {
     public_key = "${file(var.public_key_path)}"
 }
 
+data "template_file" "user-init" {
+    count = 2
+    template = "${file("${path.module}userdata.tpl")}"
+    
+    vars {
+        firewall_subnets = "${element(var.subnet_ips, count.index)}"
+    }
+}
+
 resource "aws_instance" "tf_server" {
     count         = "${var.instance_count}"
     instance_type = "${var.instance_type}"
@@ -29,5 +38,5 @@ resource "aws_instance" "tf_server" {
     key_name               = "${aws_key_pair.tf_auth.id}"
     vpc_security_group_ids = ["${var.security_group}"]
     subnet_id              = "${element(var.subnets, count.index)}"
-    user_data              = ""
+    user_data              = "${data.template_file.user-init.*.rendered[count.index]}"
 }
--- compute/variables.tf
+++ compute/variables.tf
@@ -6,3 +6,15 @@ variable "key_name" {
 variable "public_key_path" {
     default = "/home/ec2-user/.ssh/id_rsa.pub"
 }
+
+variable "subnet_ips" {
+    type = "list"
+}
+
+variable "instance_count" {}
+
+variable "instance_type" {}
+
+variable "security_group" {}
+
+variable "subnets" {}
